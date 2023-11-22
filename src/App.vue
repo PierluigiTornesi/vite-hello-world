60f4c780f55b1baf1b78184ed37c9dc63fd19578
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-22 15:34:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    components:{
+      AppTitle,
+    },
+    data(){
+      return{
+
+      }
+    },
+    methods: {
+      
+    },
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped>
+</style>
